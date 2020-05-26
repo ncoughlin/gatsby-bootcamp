e52726678be1db0bfb5c225f97e439323ac7498d
Your Name <coughlin.nicholas@gmail.com>
2020-05-26 09:13:31 -0700

    new gatsby project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Nick Coughlin</h1>
+      <h2>
+        Nisl nisi scelerisque eu ultrices vitae auctor eu. Massa tincidunt nunc
+        pulvinar sapien et ligula ullamcorper malesuada. Tellus in hac habitasse
+        platea dictumst vestibulum rhoncus est. Blandit volutpat maecenas
+        volutpat blandit aliquam etiam erat. Netus et malesuada fames ac.
+        Egestas maecenas pharetra convallis posuere. At lectus urna duis
+        convallis convallis tellus. A scelerisque purus semper eget duis. At
+        elementum eu facilisis sed odio morbi quis commodo odio. Nunc aliquet
+        bibendum enim facilisis gravida neque. Eget nunc lobortis mattis aliquam
+        faucibus purus in. Mattis vulputate enim nulla aliquet porttitor lacus
+        luctus accumsan tortor. Mattis vulputate enim nulla aliquet porttitor.
+        Neque laoreet suspendisse interdum consectetur libero id.
+      </h2>
+    </div>
+  )
 }
+
+export default IndexPage
