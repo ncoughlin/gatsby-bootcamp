e332b046d8e1bf5564ea2a35aa64edbdb8d0f660
Your Name <coughlin.nicholas@gmail.com>
2020-05-28 20:05:10 -0700

    new styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { graphql, useStaticQuery } from "gatsby";
 
+import footerStyles from './footer.module.scss'
+
 const Footer = () => {
     const data = useStaticQuery(graphql`
     query {
@@ -12,7 +14,7 @@ const Footer = () => {
     }
   `)
     return (
-        <footer>
+        <footer className={footerStyles.footer}>
            <p> Made with <span role='img' aria-label='coffee'>☕</span> by {data.site.siteMetadata.author}</p>
         </footer>
     );
