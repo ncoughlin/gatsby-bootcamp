549eb910a451c42c61740ec7f2dad0a062854142
Your Name <coughlin.nicholas@gmail.com>
2020-05-30 10:00:01 -0700

    rendering content except images

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,22 +1,28 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 import Layout from "../components/layout"
 
+// exporting graphql query returns the individual post
+// with matching slug string as a prop
 export const query = graphql`
   query($slug: String!) {
     contentfulBlogPost (slug: {eq: $slug}) {
       title
       publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
+      }
     }
   }
 `
-
 const Blog = props => {
   return (
     <Layout>
       <h1>{props.data.contentfulBlogPost.title}</h1>
       <p>{props.data.contentfulBlogPost.publishedDate}</p>
+      { documentToReactComponents(props.data.contentfulBlogPost.body.json) }
     </Layout>
   )
 }
